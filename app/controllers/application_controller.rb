7b3924e026dfd036bc407f73a6601a1fd3283025
Draconian_Devil <pavan.trek@gmail.com>
2016-07-05 11:40:26 +0530

    custom people controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,33 @@
+#
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
+  # include Pundit
   protect_from_forgery with: :exception
+  # rescue_from Pundit::NotAuthorizedError, with: :person_not_authorized
+
+  # protected
+
+  # def authenticate_manager!
+  #   redirect_to new_person_session_path unless admin? || editor?
+  # end
+
+  # def person_not_authorized
+  #   flash[:alert] = 'You are not authorized to perform this action.'
+  #   redirect_to(request.referrer || root_path)
+  # end
+
+  # def authenticate_admin!
+  #   redirect_to new_person_session_path unless admin?
+  # end
+
+  # def pundit_user
+  #   Person.find_by_id(current_person)
+  # end
+
+  # def admin?
+  #   current_person && current_person.admin?
+  # end
+
+  # def editor?
+  #   current_person && current_person.editor?
+  # end
 end
---
+++ app/controllers/people_controller.rb
@@ -0,0 +1,28 @@
+#
+class PeopleController < ApplicationController
+  before_action :set_person,            only: [:show, :destroy]
+  # before_action :authenticate_person!,  only: [:show]
+  # before_action :authenticate_manager!, only: [:index]
+  # after_action  :verify_authorized
+
+  def index
+    # authorize Person
+    @people = Person.all
+  end
+
+  def show
+    # authorize @person
+  end
+
+  def destroy
+    # authorize @person
+    @person.destroy
+    redirect_to people_path, notice: 'The account was successfully deleted'
+  end
+
+  private
+
+  def set_person
+    @person = Person.find(params[:id])
+  end
+end
