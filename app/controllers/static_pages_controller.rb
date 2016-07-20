3da9528408a8fdbc9f06d7707d0bbe75d7ac55f5
Draconian_Devil <pavan.trek@gmail.com>
2016-07-20 22:23:20 +0530

    Add handcrafted categories

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,9 +1,31 @@
 class StaticPagesController < ApplicationController
-  before_action :beautify_url
 
   def home
-    @activities = Activity.limit(6).includes(:destination, :category)
+    @activities = Activity.limit(6).includes(:destination)
     @destinations = Destination.order("created_at desc").limit(6)
+  end
+
+  def handcrafted
+    @weekend_getaways = Activity.handcrafted(:weekend_getaway).latest(6)
+    @team_outings = Activity.handcrafted(:team_outing).latest(6)
+    @elderly_activities = Activity.handcrafted(:elderly_activity).latest(6)
+    @women_specials = Activity.handcrafted(:women_special).latest(6)
+  end
+
+  def weekend
+    @weekend_getaways = Activity.handcrafted(:weekend_getaway)
+  end
+
+  def team_outing
+    @team_outings = Activity.handcrafted(:team_outing)
+  end
+
+  def elderly
+    @elderly_activities = Activity.handcrafted(:elderly_activity)
+  end
+
+  def women_special
+    @women_specials = Activity.handcrafted(:women_special)
   end
 
   def show
@@ -25,17 +47,5 @@ class StaticPagesController < ApplicationController
   end
 
   def trust
-  end
-
-  private
-
-  def beautify_url
-    if params[:search].present?
-      redirect_to search_url(q: params[:search][:q])
-    end
-  end
-
-  def query_term
-    params[:q] || ''
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,17 @@ Rails.application.routes.draw do
   }
 
   root 'static_pages#home'
-  get 'static_pages/about'
-  get 'static_pages/contact'
-  get 'static_pages/terms'
-  get 'static_pages/policies'
-  get 'static_pages/help'
-  get 'static_pages/trust'
+  get 'static_pages/about', path: '/about'
+  get 'static_pages/contact', path: '/contact'
+  get 'static_pages/terms', path: '/terms'
+  get 'static_pages/policies', path: '/policies'
+  get 'static_pages/help', path: '/help'
+  get 'static_pages/trust', path: '/trust'
+  get 'static_pages/handcrafted', path: '/handcrafted'
+  get 'static_pages/weekend', path: '/weekend'
+  get 'static_pages/team_outing', path: '/team_outing'
+  get 'static_pages/elderly', path: '/elderly'
+  get 'static_pages/women_special', path: '/women_special'
   get 'search' => 'search#show', as: :search
 
   namespace :api do
