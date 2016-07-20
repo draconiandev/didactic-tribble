5b20a6a0f8448480cbf133cb4d4512d653973676
Draconian_Devil <pavan.trek@gmail.com>
2016-07-21 01:52:06 +0530

    handcrafted category complete

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -3,13 +3,17 @@ class StaticPagesController < ApplicationController
   def home
     @activities = Activity.latest(6).includes(:destination)
     @destinations = Destination.order("created_at desc").limit(6)
+    @weekend_getaways = Activity.handcrafted(:weekend_getaway).count
+    @team_outings = Activity.handcrafted(:team_outing).count
+    @elderly_activities = Activity.handcrafted(:elderly_activity).count
+    @women_specials = Activity.handcrafted(:women_special).count
   end
 
   def handcrafted
-    @weekend_getaways = Activity.handcrafted(:weekend_getaway).latest(6)
-    @team_outings = Activity.handcrafted(:team_outing).latest(6)
-    @elderly_activities = Activity.handcrafted(:elderly_activity).latest(6)
-    @women_specials = Activity.handcrafted(:women_special).latest(6)
+    @weekend_getaways = Activity.handcrafted(:weekend_getaway).latest(9)
+    @team_outings = Activity.handcrafted(:team_outing).latest(9)
+    @elderly_activities = Activity.handcrafted(:elderly_activity).latest(9)
+    @women_specials = Activity.handcrafted(:women_special).latest(9)
   end
 
   def weekend
