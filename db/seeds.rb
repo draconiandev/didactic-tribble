381e083830749fd7da767729905b6ead0b4f483c
hrabhi <abhishek.trek@gmail.com>
2016-07-20 19:21:28 +0530

    abhi changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Destination.create(name: 'Coorg', description: "Mysore (or Mysuru), a city in India's Karnataka state, was the capital of the Kingdom of Mysore from 1399-1947. In its center is opulent Mysore Palace, former seat of the ruling Wodeyar dynasty. The palace blends Hindu, Islamic, Gothic and Rajput styles, and is dramatically lit at night. Mysore is also home to the centuries-old Devaraja Market, filled with spices, silk and sandalwood.", meta_description: "The palace blends Hindu, Islamic, Gothic and Rajput styles, and is dramatically lit at night.", cover: { File.open("#{Rails.root}/spec/fixtures/files/card.jpg")
