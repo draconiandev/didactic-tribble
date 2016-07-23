fca7a790602a79ceb92364f574560616477c0f39
Draconian_Devil <pavan.trek@gmail.com>
2016-07-24 04:44:40 +0530

    vendor complete

--- app/models/vendor.rb
+++ app/models/vendor.rb
@@ -3,6 +3,9 @@ class Vendor < ActiveRecord::Base
   has_many :subscriptions
   has_many :categories, through: :subscriptions
 
+  validates :name, :address, :phone, :email, presence: true
+  validates :name, length: {minimum: 3}
+
   def categories_attributes=(category_attributes)
     category_attributes.values.each do |category_attribute|
       category = Category.find_or_create_by(category_attribute)
