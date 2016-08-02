8f6ebece02a4ec5931918b66e18c6cc415528406
hrabhi <abhishek.trek@gmail.com>
2016-08-03 00:14:16 +0530

    initialize slider

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -54,4 +54,5 @@ $(document).ready(function() {
     $('.tooltipped').tooltip({delay: 50});
     $('.scrollspy').scrollSpy();
     $('.tabs-wrapper .row').pushpin({ top: $('.tabs-wrapper').offset() });
+    $('.slider').slider({full_width: true});
 });
