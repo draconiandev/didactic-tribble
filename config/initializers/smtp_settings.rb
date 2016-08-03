4fa2d94ca718a7d9663464fbcbe9789cc0aad159
Draconian_Devil <pavan.trek@gmail.com>
2016-08-03 18:21:11 +0530

    change to sendgrid

--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,10 +1,10 @@
 # Change to sendgrid for production and staging
 ActionMailer::Base.smtp_settings = {
-  address: 							'smtp.gmail.com',
+  address: 							'smtp.sendgrid.net',
   port:                 587,
   domain:               ENV['SMTP_DOMAIN'],
-  user_name:            ENV['SMTP_USERNAME'],
-  password:             ENV['SMTP_PASSWORD'],
+  user_name:            ENV['SENDGRID_USERNAME'],
+  password:             ENV['SENDGRID_PASSWORD'],
   authentication:       'plain',
   enable_starttls_auto: true
 }
