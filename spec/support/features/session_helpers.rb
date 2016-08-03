f13cf3f0440a2a2718003432b06ed44c08e5d7bf
Draconian_Devil <pavan.trek@gmail.com>
2016-08-04 01:22:38 +0530

    update travis.yml

--- spec/support/features/session_helpers.rb
+++ spec/support/features/session_helpers.rb
@@ -2,17 +2,17 @@
 module Features
   def sign_up(name, email, password)
     visit new_person_registration_path
-    fill_in 'Name', with: name
-    fill_in 'Email', with: email
-    fill_in 'Password', with: password, match: :prefer_exact
+    fill_in 'person[name]', with: name
+    fill_in 'person[email]', with: email
+    fill_in 'person[password]', with: password, match: :prefer_exact
     # click_button 'Sign up'
     find(:xpath, "//input[contains(@name, 'commit')]").click
   end
 
   def signin(email, password)
     visit new_person_session_path
-    fill_in 'Email', with: email
-    fill_in 'Password', with: password
+    fill_in 'person[email]', with: email
+    fill_in 'person[password]', with: password
     # click_button 'Sign in'
     find(:xpath, "//input[contains(@name, 'commit')]").click
   end
