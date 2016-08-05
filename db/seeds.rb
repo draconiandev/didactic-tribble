10d0aafd286f3e7ed3cf9e073a914c7fa89ab0ee
Draconian_Devil <pavan.trek@gmail.com>
2016-08-06 00:17:23 +0530

    seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,6 @@
 # Destination.create(name: 'Coorg', description: "Mysore (or Mysuru), a city in India's Karnataka state, was the capital of the Kingdom of Mysore from 1399-1947. In its center is opulent Mysore Palace, former seat of the ruling Wodeyar dynasty. The palace blends Hindu, Islamic, Gothic and Rajput styles, and is dramatically lit at night. Mysore is also home to the centuries-old Devaraja Market, filled with spices, silk and sandalwood.",
 #                    meta_description: "The palace blends Hindu, Islamic, Gothic and Rajput styles, and is dramatically lit at night.",
 #                    cover: { File.open("#{Rails.root}/spec/fixtures/files/card.jpg")})
-Person.create(name: 'Abhishek', email: 'abhishek@trekhub.in', password: 'abhishek78', password_confirmation: 'abhishek78', role: 'admin')
+Person.create(name: 'Pavan', email: 'pavan@trekhub.in', password: 'pavanp12', password_confirmation: 'pavanp12', role: 'admin')
+Person.create(name: 'Abhishek', email: 'abhishek@trekhub.in', password: 'abhishek12', password_confirmation: 'abhishek12', role: 'admin')
+Person.create(name: 'Shomika', email: 'shomika@trekhub.in', password: 'shomika12', password_confirmation: 'shomika12', role: 'admin')
