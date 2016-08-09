b19f84b5cc70d6fbbaf7f87cd54e88a339d2bd39
Draconian_Devil <pavan.trek@gmail.com>
2016-08-09 05:48:18 +0530

    revert back to default devise routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :people, skip: [:sessions, :passwords, :confirmations, :registrations],
-                      controllers: {
+  devise_for :people, controllers: {
                         registrations: 'people/registrations',
                         omniauth_callbacks: 'people/omniauth_callbacks'
                       }
@@ -45,37 +44,4 @@ Rails.application.routes.draw do
 
   match '/contact', to: 'contacts#new', via: 'get'
   resources "contacts", only: [:new, :create]
-
-  as :person do
-    # session handling
-    get     '/login'  => 'devise/sessions#new',     as: 'new_person_session'
-    post    '/login'  => 'devise/sessions#create',  as: 'person_session'
-    delete  '/logout' => 'devise/sessions#destroy', as: 'destroy_person_session'
-
-    # joining
-    get   '/join' => 'devise/registrations#new',    as: 'new_person_registration'
-    post  '/join' => 'devise/registrations#create', as: 'person_registration'
-
-    scope '/account' do
-      # password reset
-      get   '/reset-password'        => 'devise/passwords#new',    as: 'new_person_password'
-      put   '/reset-password'        => 'devise/passwords#update', as: 'person_password'
-      post  '/reset-password'        => 'devise/passwords#create'
-      get   '/reset-password/change' => 'devise/passwords#edit',   as: 'edit_person_password'
-
-      # confirmation
-      get   '/confirm'        => 'devise/confirmations#show',   as: 'person_confirmation'
-      post  '/confirm'        => 'devise/confirmations#create'
-      get   '/confirm/resend' => 'devise/confirmations#new',    as: 'new_person_confirmation'
-
-      # settings & cancellation
-      get '/cancel'   => 'devise/registrations#cancel', as: 'cancel_person_registration'
-      get '/settings' => 'devise/registrations#edit',   as: 'edit_person_registration'
-      put '/settings' => 'devise/registrations#update'
-
-      # account deletion
-      delete '' => 'devise/registrations#destroy'
-    end
-  
-  end
 end
