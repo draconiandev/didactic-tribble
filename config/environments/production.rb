81b79d80fb9e61b34ae9fa133a2c9d2938da28cc
Draconian_Devil <pavan.trek@gmail.com>
2016-08-09 06:06:22 +0530

    provide host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,4 +13,5 @@ Rails.application.configure do
   config.log_formatter = ::Logger::Formatter.new
   config.active_record.dump_schema_after_migration = false
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { host: 'www.trekhub.in' }
 end
--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -2,7 +2,6 @@
 ActionMailer::Base.smtp_settings = {
   address: 							'smtp.sendgrid.net',
   port:                 587,
-  host:                 'trekhub.in'
   domain:               ENV['SMTP_DOMAIN'],
   user_name:            ENV['SENDGRID_USERNAME'],
   password:             ENV['SENDGRID_PASSWORD'],
