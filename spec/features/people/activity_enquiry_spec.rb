1c8dee3de32b211844bc81420fdcb38d55d28c5c
Draconian_Devil <pavan.trek@gmail.com>
2016-08-20 03:06:14 +0530

    remove activity enquiry spec

--- spec/features/people/activity_enquiry_spec.rb
+++ spec/features/people/activity_enquiry_spec.rb
@@ -1,30 +1,30 @@
 feature 'Registered user' do
   
-  before do
-    login_as(create(:person, :user))
-  end
+  # before do
+  #   login_as(create(:person, :user))
+  # end
 
-  scenario 'can enquire about an activity successfully with valid attributes', js:true do
-    activity = create(:activity)
-    visit activities_path
-    click_link activity.title
-    click_button 'Enquire'
-    fill_in 'Phone', with: '+919611201234'
-    fill_in 'Message', with: 'Some requirements'
-    click_button 'Submit'
+  # scenario 'can enquire about an activity successfully with valid attributes', js:true do
+  #   activity = create(:activity)
+  #   visit activities_path
+  #   click_link activity.title
+  #   click_button 'Enquire'
+  #   fill_in 'Phone', with: '+919611201234'
+  #   fill_in 'Message', with: 'Some requirements'
+  #   click_button 'Submit'
 
-    expect(page).to have_content('Thank you for your enquiry. We will get back soon to help you in your plans!')
-  end
+  #   expect(page).to have_content('Thank you for your enquiry. We will get back soon to help you in your plans!')
+  # end
 
-  scenario 'cannot enquire about an activity with invalid attributes', js:true do
-    activity = create(:activity)
-    visit activities_path
-    click_link activity.title
-    click_button 'Enquire'
-    fill_in 'Phone', with: ''
-    fill_in 'Message', with: 'Some requirements'
-    click_button 'Submit'
+  # scenario 'cannot enquire about an activity with invalid attributes', js:true do
+  #   activity = create(:activity)
+  #   visit activities_path
+  #   click_link activity.title
+  #   click_button 'Enquire'
+  #   fill_in 'Phone', with: ''
+  #   fill_in 'Message', with: 'Some requirements'
+  #   click_button 'Submit'
 
-    expect(page).to have_content('Sorry! We could not deliver the message. Please try again')
-  end
+  #   expect(page).to have_content('Sorry! We could not deliver the message. Please try again')
+  # end
 end
