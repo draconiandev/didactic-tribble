2197a0e62ff988ed24d2dcb13c45fef7433dc53b
Draconian_Devil <pavan.trek@gmail.com>
2016-10-16 11:20:19 +0530

    add relations and friendly id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,8 @@
 class Article < ActiveRecord::Base
+  has_many :galleries
+  accepts_nested_attributes_for :galleries, reject_if: :all_blank, allow_destroy: true
+
+  validates :title, uniqueness: { case_sensitive: false }
+
+  friendly_id :title, use: [:slugged, :finders, :history]
 end
--- app/models/gallery.rb
+++ app/models/gallery.rb
@@ -1,6 +1,7 @@
 class Gallery < ActiveRecord::Base
   mount_uploader :image, ImageUploader
   belongs_to :activity
+  belongs_to :article
 
   validates :image, presence: true
 end
